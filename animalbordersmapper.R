3cd5d33d54b4d2e0e948e424445af4e783d46c1e
cndesantana <charles.santana@gmail.com>
2019-03-14 13:24:18 +0100

    Reading the taxa input files

--- animalbordersmapper.R
+++ animalbordersmapper.R
@@ -10,14 +10,29 @@
 ##################
 library(xlsx)
 library(readxl)
+library(dplyr)
 
 workdir <- "/home/charles/ETH/gasm/realData/albouy_data/preprocessing/animalbordersmapper/taxadata"
 setwd(workdir)
 #To Read the taxa data files
-csvfiles <- system("ls *.csv",intern=TRUE)
-xlsxfiles <- system("ls *.xlsx",intern=TRUE)
+df_taxa <- data.frame(order=character(0)
+                      ,genus=character(0)
+                      ,species=character(0))
+amphibtaxa <- read.delim("amphib_names.csv",sep="\t")
+df_taxa <- rbind(df_taxa, 
+                 amphibtaxa %>% 
+                   mutate(speciesname = paste(genus, species)) %>% 
+                   select(genus, species, speciesname))
 
+clementscheck <- read_xlsx("Clements-Checklist-v2018-August-2018.xlsx")
+df_taxa <- rbind(df_taxa,
+                 clementscheck %>% 
+                   mutate(species = `scientific name`) %>% 
+                   dplyr::select(order, species))
 
+mammals <- read_xlsx("global-mammal-checklist-at-may-2018.xlsx", skip = 3)
+
+reptiles <- read.delim("taxa_reptiles.csv",sep="\t")
 
 #To Read the shapefile containing presence-absence of species
 
